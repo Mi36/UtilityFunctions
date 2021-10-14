9fe3c896f10ea826d3c57b6d7a4bef3b54011cc6
Illyas <illyas.deulect@gmail.com>
2021-10-14 20:02:38 +0530

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,13 @@
+const bubbleSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const lesser = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = lesser;
+      }
+    }
+  }
+  return arr;
+};
+console.log(bubbleSort([25, 36, 25, 369]));
