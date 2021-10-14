a29a9b920c341163bd642ede6088af60d079113a
Illyas <illyas.deulect@gmail.com>
2021-10-14 20:03:12 +0530

    staircase problem

--- pyramid.js
+++ pyramid.js
@@ -1,9 +1,12 @@
 const pyramid = (n) => {
+  let a = 0;
   for (let row = 0; row < n; row++) {
     let stair = "";
+
     for (let col = 0; col < n; col++) {
       if (col <= row) {
-        stair = stair + "*";
+        a = a + 1;
+        stair = stair + a;
       } else {
         stair = stair + " ";
       }
@@ -11,20 +14,20 @@ const pyramid = (n) => {
     console.log(stair);
   }
 };
-const pyramid = (n) => {
-  const midpoint = Math.floor((2 * n - 1) / 2);
-  console.log(midpoint, (2 * n - 1) / 2);
-  for (let row = 0; row < n; row++) {
-    let stair = "";
-    for (let col = 0; col < 2 * n - 1; col++) {
-      if (col < midpoint - row || col > midpoint + row) {
-        stair = stair + " ";
-      } else {
-        stair = stair + "*";
-      }
-    }
-    console.log(stair);
-  }
-};
+// const pyramid = (n) => {
+//   const midpoint = Math.floor((2 * n - 1) / 2);
+//   console.log(midpoint, (2 * n - 1) / 2);
+//   for (let row = 0; row < n; row++) {
+//     let stair = "";
+//     for (let col = 0; col < 2 * n - 1; col++) {
+//       if (col < midpoint - row || col > midpoint + row) {
+//         stair = stair + " ";
+//       } else {
+//         stair = stair + "*";
+//       }
+//     }
+//     console.log(stair);
+//   }
+// };
 
-pyramid(2);
+pyramid(20);
